3fbf5eec86e68bad3313b4f4bfba97cf345007dc
leocoiradas <coiradasalan@gmail.com>
2024-01-24 18:50:48 -0300

    Añadida clase Librerías

---
+++ Logica/Library/Libraries.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.Library
+{
+    public class Libraries
+    {
+        public UploadImage uploadImage = new UploadImage();
+        public TextBoxEvent textBoxEvent = new TextBoxEvent();
+    }
+}
