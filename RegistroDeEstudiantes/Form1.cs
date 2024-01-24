51376a9bdab6ca97ed6909b9d52fc472cfd81a71
leocoiradas <coiradasalan@gmail.com>
2024-01-24 18:53:10 -0300

    Form1: Listado de métodos para cada campo de texto según su necesidad
    Fecha de nacimiento, nombre, apellido, id, etc.

--- RegistroDeEstudiantes/Form1.cs
+++ RegistroDeEstudiantes/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Logica;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,131 @@ namespace RegistroDeEstudiantes
 {
     public partial class Form1 : Form
     {
+        private EstudiantesLogica estudiantes;
         public Form1()
         {
             InitializeComponent();
+            //La sig lista se crea para obtener la información de los campos de texto
+            var listTextBox = new List <TextBox> ();
+            listTextBox.Add(TextBoxID);
+            listTextBox.Add(TextBoxName);
+            listTextBox.Add(TextBoxLastName);
+            listTextBox.Add(TextBoxDegree);
+            listTextBox.Add(TextBoxEmail);
+            var listLabel = new List <Label> ();
+            listLabel.Add(labelID);
+            listLabel.Add(labelName);
+            listLabel.Add(labelLastName);
+            listLabel.Add(labelDegree);
+            listLabel.Add(labelEmail);
+            Object[] images = { PictureBoxProfile };
+            estudiantes = new EstudiantesLogica(listTextBox, listLabel, images);
         }
+
+
+        private void PictureBoxProfile_Click(object sender, EventArgs e)
+        {
+            estudiantes.uploadImage.LoadImage(PictureBoxProfile);
+        }
+
+        private void TextBoxID_TextChanged(object sender, EventArgs e)
+        {
+            if (TextBoxID.Text.Equals(""))
+            {
+                labelID.ForeColor = Color.Red;
+            }
+            else
+            {
+                labelID.ForeColor = Color.Green;
+                labelID.Text = "ID";
+            }
+        }
+
+        private void TextBoxID_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            estudiantes.textBoxEvent.numberKeyPress(e);
+        }
+
+        private void TextBoxLastName_TextChanged(object sender, EventArgs e)
+        {
+            if (TextBoxLastName.Text.Equals(""))
+            {
+                labelLastName.ForeColor = Color.Red;
+            }
+            else
+            {
+                labelLastName.ForeColor = Color.Green;
+                labelLastName.Text = "Apellido";
+            }
+        }
+        private void TextBoxLastName_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            estudiantes.textBoxEvent.textKeyPress(e);
+        }
+
+        private void TextBoxName_TextChanged_1(object sender, EventArgs e)
+        {
+            if (TextBoxName.Text.Equals(""))
+            {
+                labelName.ForeColor = Color.Red;
+            }
+            else
+            {
+                labelName.ForeColor = Color.Green;
+                labelName.Text = "Nombre";
+            }
+        }
+
+        private void TextBoxName_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            estudiantes.textBoxEvent.textKeyPress(e);
+
+        }
+
+        private void TextBoxDegree_TextChanged_1(object sender, EventArgs e)
+        {
+            if (TextBoxDegree.Text.Equals(""))
+            {
+                labelDegree.ForeColor = Color.Red;
+            }
+            else
+            {
+                labelDegree.ForeColor = Color.Green;
+                labelDegree.Text = "Carrera";
+            }
+        }
+
+        private void TextBoxDegree_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            estudiantes.textBoxEvent.textKeyPress(e);
+        }
+
+        private void TextBoxEmail_TextChanged(object sender, EventArgs e)
+        {
+
+            if (TextBoxEmail.Text.Equals(""))
+            {
+                labelEmail.ForeColor = Color.Red;
+            }
+            else
+            {
+                labelEmail.ForeColor = Color.Green;
+                labelEmail.Text = "Email";
+            }
+        }
+
+        private void TextBoxEmail_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            estudiantes.textBoxEvent.textKeyPress(e);
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            estudiantes.Registrar();
+        }
+
+        
     }
 }
+
+    
