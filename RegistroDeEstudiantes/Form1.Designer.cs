6090f3f2d8849c3964ae165a51f59fe53296e51f
leocoiradas <coiradasalan@gmail.com>
2024-02-09 10:33:37 -0300

    Form1.Designer: Añadidos botones controladores del paginador
    Se añadieron los botones correspondientes al paginador próximo a implementar, se cambiaron los valores de los label de cada botón. Se creó el numericUpDown para marcar el número de página actual y se le dio varios estilos. Adicionalmente se le dio estilos a las filas de datos mostradas en el datagridView.

--- RegistroDeEstudiantes/Form1.Designer.cs
+++ RegistroDeEstudiantes/Form1.Designer.cs
@@ -58,6 +58,12 @@ namespace RegistroDeEstudiantes
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.textBoxSearch = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.buttonFirst = new System.Windows.Forms.Button();
+            this.buttonPrev = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonLast = new System.Windows.Forms.Button();
+            this.labelPages = new System.Windows.Forms.Label();
+            this.numPage = new System.Windows.Forms.NumericUpDown();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox5.SuspendLayout();
@@ -65,6 +71,7 @@ namespace RegistroDeEstudiantes
             this.groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPage)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -326,6 +333,12 @@ namespace RegistroDeEstudiantes
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox3.BackColor = System.Drawing.Color.White;
+            this.groupBox3.Controls.Add(this.numPage);
+            this.groupBox3.Controls.Add(this.labelPages);
+            this.groupBox3.Controls.Add(this.buttonLast);
+            this.groupBox3.Controls.Add(this.buttonNext);
+            this.groupBox3.Controls.Add(this.buttonPrev);
+            this.groupBox3.Controls.Add(this.buttonFirst);
             this.groupBox3.Controls.Add(this.dataGridView1);
             this.groupBox3.Controls.Add(this.label9);
             this.groupBox3.Location = new System.Drawing.Point(479, 155);
@@ -340,12 +353,15 @@ namespace RegistroDeEstudiantes
             // 
             this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(14, 93);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(565, 361);
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(565, 397);
             this.dataGridView1.TabIndex = 1;
             // 
             // label9
@@ -383,6 +399,7 @@ namespace RegistroDeEstudiantes
             this.textBoxSearch.Name = "textBoxSearch";
             this.textBoxSearch.Size = new System.Drawing.Size(492, 35);
             this.textBoxSearch.TabIndex = 1;
+            this.textBoxSearch.TextChanged += new System.EventHandler(this.textBoxSearch_TextChanged);
             // 
             // label2
             // 
@@ -394,6 +411,92 @@ namespace RegistroDeEstudiantes
             this.label2.Size = new System.Drawing.Size(87, 29);
             this.label2.TabIndex = 0;
             this.label2.Text = "Buscar";
+            // 
+            // buttonFirst
+            // 
+            this.buttonFirst.BackColor = System.Drawing.Color.Aqua;
+            this.buttonFirst.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonFirst.FlatAppearance.BorderSize = 0;
+            this.buttonFirst.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.buttonFirst.FlatAppearance.MouseOverBackColor = System.Drawing.Color.RoyalBlue;
+            this.buttonFirst.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonFirst.ForeColor = System.Drawing.Color.Black;
+            this.buttonFirst.Location = new System.Drawing.Point(121, 532);
+            this.buttonFirst.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonFirst.Name = "buttonFirst";
+            this.buttonFirst.Size = new System.Drawing.Size(75, 28);
+            this.buttonFirst.TabIndex = 8;
+            this.buttonFirst.Text = "Primero";
+            this.buttonFirst.UseVisualStyleBackColor = false;
+            // 
+            // buttonPrev
+            // 
+            this.buttonPrev.BackColor = System.Drawing.Color.Aqua;
+            this.buttonPrev.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonPrev.FlatAppearance.BorderSize = 0;
+            this.buttonPrev.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.buttonPrev.FlatAppearance.MouseOverBackColor = System.Drawing.Color.RoyalBlue;
+            this.buttonPrev.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPrev.ForeColor = System.Drawing.Color.Black;
+            this.buttonPrev.Location = new System.Drawing.Point(204, 532);
+            this.buttonPrev.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonPrev.Name = "buttonPrev";
+            this.buttonPrev.Size = new System.Drawing.Size(83, 28);
+            this.buttonPrev.TabIndex = 9;
+            this.buttonPrev.Text = "Anterior";
+            this.buttonPrev.UseVisualStyleBackColor = false;
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.BackColor = System.Drawing.Color.Aqua;
+            this.buttonNext.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonNext.FlatAppearance.BorderSize = 0;
+            this.buttonNext.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.buttonNext.FlatAppearance.MouseOverBackColor = System.Drawing.Color.RoyalBlue;
+            this.buttonNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNext.ForeColor = System.Drawing.Color.Black;
+            this.buttonNext.Location = new System.Drawing.Point(295, 533);
+            this.buttonNext.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(76, 28);
+            this.buttonNext.TabIndex = 10;
+            this.buttonNext.Text = "Siguiente";
+            this.buttonNext.UseVisualStyleBackColor = false;
+            // 
+            // buttonLast
+            // 
+            this.buttonLast.BackColor = System.Drawing.Color.Aqua;
+            this.buttonLast.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonLast.FlatAppearance.BorderSize = 0;
+            this.buttonLast.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.buttonLast.FlatAppearance.MouseOverBackColor = System.Drawing.Color.RoyalBlue;
+            this.buttonLast.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLast.ForeColor = System.Drawing.Color.Black;
+            this.buttonLast.Location = new System.Drawing.Point(379, 533);
+            this.buttonLast.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonLast.Name = "buttonLast";
+            this.buttonLast.Size = new System.Drawing.Size(76, 28);
+            this.buttonLast.TabIndex = 11;
+            this.buttonLast.Text = "Último";
+            this.buttonLast.UseVisualStyleBackColor = false;
+            // 
+            // labelPages
+            // 
+            this.labelPages.AutoSize = true;
+            this.labelPages.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPages.Location = new System.Drawing.Point(256, 493);
+            this.labelPages.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelPages.Name = "labelPages";
+            this.labelPages.Size = new System.Drawing.Size(90, 25);
+            this.labelPages.TabIndex = 12;
+            this.labelPages.Text = "Páginas";
+            // 
+            // numPage
+            // 
+            this.numPage.Location = new System.Drawing.Point(7, 539);
+            this.numPage.Name = "numPage";
+            this.numPage.Size = new System.Drawing.Size(82, 22);
+            this.numPage.TabIndex = 13;
             // 
             // Form1
             // 
@@ -419,6 +522,7 @@ namespace RegistroDeEstudiantes
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.groupBox4.ResumeLayout(false);
             this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPage)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -452,6 +556,12 @@ namespace RegistroDeEstudiantes
         private System.Windows.Forms.TextBox textBoxSearch;
         private System.Windows.Forms.PictureBox PictureBoxProfile;
         private DataGridView dataGridView1;
+        private NumericUpDown numPage;
+        private Label labelPages;
+        private Button buttonLast;
+        private Button buttonNext;
+        private Button buttonPrev;
+        private Button buttonFirst;
     }
 }
 
